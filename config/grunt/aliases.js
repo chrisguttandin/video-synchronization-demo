7bc71546f1ab54cc2ae580f4259501e98c7e8b6d
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-01-03 22:23:13 +0100

    build(config): update grunt config

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,15 +1,7 @@
-const { env } = require('process');
-
-// eslint-disable-next-line padding-line-between-statements
-const filter = (tasks) => tasks.filter((task) => task !== null);
-const isVersionUpdate =
-    env.TRAVIS === 'true' && env.TRAVIS_PULL_REQUEST === 'false' && env.TRAVIS_SECURE_ENV_VARS === 'true' && env.TRAVIS_TAG !== '';
-
 module.exports = {
     'build:development': ['clean', 'htmlmin', 'webpack:development', 'sh:build-development', 'copy:assets'],
     'build:production': ['clean', 'htmlmin', 'webpack:production', 'sh:build-production', 'copy:assets'],
     'deploy': ['build:production', 'gh-pages:deploy'],
-    'deploy-on-version-updates': filter([isVersionUpdate ? 'deploy' : null]),
     'lint': ['sh:lint-config', 'sh:lint-src'],
     'monitor': ['build:development', 'connect', 'watch:development'],
     'preview': ['build:production', 'connect', 'watch:production'],
