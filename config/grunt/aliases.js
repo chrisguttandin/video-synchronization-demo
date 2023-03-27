b9134d22a85a03ddc18447e2ddec5c63c130c2b2
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-28 01:02:27 +0200

    build(config): replace grunt config with npm scripts

--- config/grunt/aliases.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    build: ['sh:clean', 'sh:html-minifier', 'sh:build'],
-    lint: ['sh:lint-config', 'sh:lint-src'],
-    monitor: ['sh:monitor'],
-    smoke: ['sh:hyperlink']
-};
